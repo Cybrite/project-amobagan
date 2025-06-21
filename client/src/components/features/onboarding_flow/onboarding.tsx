4177932328fd05571336a5e3568b6e44dde3fadb
Harsh <harshtanishq2002@gmail.com>
2025-06-21 10:33:35 +0530

    feat: added landing page

--- client/src/components/features/onboarding_flow/onboarding.tsx
+++ client/src/components/features/onboarding_flow/onboarding.tsx
@@ -74,6 +74,7 @@ const OnboardingFlow = () => {
       <div className="absolute inset-0 bg-[url('data:image/svg+xml;charset=utf-8,%3Csvg%20width%3D%2260%22%20height%3D%2260%22%20viewBox%3D%220%200%2060%2060%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cg%20fill%3D%22none%22%20fill-rule%3D%22evenodd%22%3E%3Cg%20fill%3D%22%239C92AC%22%20fill-opacity%3D%220.05%22%3E%3Ccircle%20cx%3D%2230%22%20cy%3D%2230%22%20r%3D%221%22%2F%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E')] opacity-20"></div>
 
       <div className="relative w-full max-w-xl">
+
         <StepIndicator currentStep={currentStep} totalSteps={5} />
 
         {currentStep === 1 && (
