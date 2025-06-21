b2a8c068946cbadeace6b41df8680cd56902d998
Harsh <harshtanishq2002@gmail.com>
2025-06-22 00:19:26 +0530

    fix: fixed step indicator alignment

--- client/src/components/features/onboarding_flow/components/StepIndicator.tsx
+++ client/src/components/features/onboarding_flow/components/StepIndicator.tsx
@@ -45,37 +45,38 @@ const StepIndicator = ({
                 strokeWidth={2}
                 d="M15 19l-7-7 7-7"
               />
-            </svg>
+            </svg>{" "}
             Back
           </button>
         </div>
-      )}
-
+      )}{" "}
       {/* Progress Indicator - Clean and centered */}
       <div className="flex items-center justify-center">
-        <div className="relative w-full max-w-80 h-3 bg-gray-200 rounded-full overflow-hidden">
+        <div className="relative w-full max-w-80">
+          {/* Progress bar head image - positioned above the bar */}
           <div
-            className="absolute top-0 left-0 h-full transition-all duration-500 ease-out bg-[#004743] rounded-full"
+            className="absolute -top-2.5 transform -translate-x-1/2 transition-all duration-500 ease-out z-10"
             style={{
-              width: `${progressPercentage}%`,
-            }}
-          />
-
-          <div
-            className="absolute top-1/2 transform -translate-y-1/2 w-6 h-6 bg-[#004743] rounded-full flex items-center justify-center transition-all duration-500 ease-out shadow-lg border-2 border-white"
-            style={{
-              left: `calc(${progressPercentage}% - 12px)`,
+              left: `${progressPercentage}%`,
             }}
           >
             <img
               src="https://res.cloudinary.com/dqqyuvg1v/image/upload/v1750475065/Vector_ug5wsz.png"
-              alt="progress indicator"
-              className="w-3 h-3"
+              alt="Progress indicator"
+              className="w-6 h-6"
+            />
+          </div>
+          {/* Progress bar */}
+          <div className="relative w-full h-2 bg-gray-200 rounded-full overflow-hidden">
+            <div
+              className="absolute top-0 left-0 h-full transition-all duration-500 ease-out bg-[#004743] rounded-full"
+              style={{
+                width: `${progressPercentage}%`,
+              }}
             />
           </div>
         </div>
       </div>
-
       {/* Step counter */}
       <div
         className={`text-center text-sm ${
